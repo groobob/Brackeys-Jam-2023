dcb6ca4f013294f8823ae6004a014ee52edfec39
ZacharyH777 <93843297+ZacharyH777@users.noreply.github.com>
2023-08-26 15:54:58 -0700

    UpdatedFish
    Updated Fish with more fish
    
    Still needs odds change

--- Assets/Scripts/SetUpTypes/CreateEnemyType.cs
+++ Assets/Scripts/SetUpTypes/CreateEnemyType.cs
@@ -11,6 +11,9 @@ public class CreateEnemyType : MonoBehaviour
     private int fishSpeed = 0;
 
     private Transform playerLoc;
+    private float changingForms = 1;
+    private bool inChange = false;
+
 
     private int facing;
     // Start is called before the first frame update
@@ -21,18 +24,25 @@ public class CreateEnemyType : MonoBehaviour
         rb = this.GetComponent<Rigidbody2D>();
 
         int randomtype = Random.Range(0,100);
-
-        if(randomtype >= 0 && randomtype < 34)
+        if(randomtype >= 1 - (playerLoc.position.y/200) && randomtype < 34)
             randomtype = 1;
 
-        if(randomtype >= 33 && randomtype < 95)
+        else if(randomtype >= 33 && randomtype < 37 - (playerLoc.position.y/50))
+            randomtype = 3;
+
+        else if(randomtype >= 37 - (playerLoc.position.y/50) && randomtype < 99)
             randomtype = 0;
-        if(playerLoc.position.y < -70)
+
+        else if(playerLoc.position.y < -70)
         {
-            if(randomtype >= 95 && randomtype < 100)
+            if(randomtype >= 98 && randomtype < 100)
                 randomtype = 2;
+            if(randomtype >= 0 && randomtype < 1 - (playerLoc.position.y/200))
+                randomtype = 3;
         }    
-        else if (randomtype >= 95 && randomtype < 100)
+        else if (randomtype >= 99 && randomtype < 100)
+            randomtype = 0;
+        else if(randomtype >= 0 && randomtype < 1 - (playerLoc.position.y/200))
             randomtype = 1;
         
         enemyType = this.transform.parent.gameObject.GetComponent<listAndAccessToEnemyTypes>().getEnemyType(randomtype);
@@ -48,6 +58,10 @@ public class CreateEnemyType : MonoBehaviour
             sprtR.color = new Color(1f,0f,0f,1);
             this.gameObject.layer = 7;
         }
+
+        if(enemyType.EnemyName == "AppearFish")
+            sprtR.color = new Color(.7f,.32f,.5f,1);
+
         fishSpeed = enemyType.speed;
 
         if(facing == 0)
@@ -64,6 +78,37 @@ public class CreateEnemyType : MonoBehaviour
         rb.velocity = new Vector2(facing * fishSpeed, 0);
         if(this.transform.position.y - 40 > playerLoc.position.y)
             Destroy(this.gameObject);
+
+        if(enemyType.hasSpikes)
+        {
+            if(changingForms < -.5f)
+            {
+                inChange = true;
+            }
+            else if(changingForms > 1f)
+            {
+                inChange = false;
+            }
+            if(inChange == false)
+            {
+                sprtR.color = new Color(.7f,.32f,.5f,changingForms);
+                changingForms -= .001f;
+            }
+            if(inChange == true)
+            {
+                sprtR.color = new Color(.7f,.32f,.5f,changingForms);
+                changingForms += .001f;
+            }
+
+            if(changingForms >= 0f)
+            {
+                this.gameObject.layer = 8;
+            }
+            else
+            {
+                this.gameObject.layer = 9;
+            }
+        }
     }
 
     void FixedUpdate()
